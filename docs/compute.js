b9b4f4b5614d079fe979134421d1cb526f2df90e
Rutuparn Pawar <rutuparn.17U253@viit.ac.in>
2020-05-11 14:46:19 +0530

    /docs folder created

---
+++ docs/compute.js
@@ -0,0 +1,221 @@
+// Quiz API for adding quizzes to a webpage
+// Written by Rutuparn Pawar (InputBlackBoxOutput)
+
+/////////////////////////////////////////////////////////////////////////////////////
+const quest = document.getElementById('question');
+const opt1_ = document.getElementById('opt1');
+const opt2_ = document.getElementById('opt2');
+const opt3_ = document.getElementById('opt3');
+const opt4_ = document.getElementById('opt4');
+
+const score_ = document.getElementById('score');
+const result_ = document.getElementById('result');
+const prog_ = document.getElementById('prog-bar');
+
+/////////////////////////////////////////////////////////////////////////////////////
+class Quiz{
+	constructor(Qbank) {
+		this.rand = 0;
+		this.score = 0;
+		this.result ='Something went wrong!';	
+		
+		this.enablePrevButton = false;
+		this.isElementHidden('prev', !this.enablePrevButton);
+
+		if(Qbank == null) 
+			this.questionBank = this.getTestQuestionBank();
+		else
+			this.questionBank = JSON.parse(Qbank);
+		
+		this.numberOfquestions = this.questionBank.results.length;
+		this.currentQuestion = 0;
+		this.answered = false;
+
+		score_.innerText = `Score: ${this.score}`;
+	}
+
+	greetUser(name) {
+		document.getElementById('greet').innerText = 'Hello ' + name + ' !';
+	}
+
+	generateRandomNumber = () => Math.floor(Math.random() * 4); 
+		
+	displayQuestionAndOptions(questionNo) {
+		result_.innerText = "";
+
+		this.isElementHidden('next', true);
+		this.answered = false;
+
+		opt1_.style.cssText += "background-color: #FA2A4A";  //#E21B3C
+		opt2_.style.cssText += "background-color: #2A7ADA";  //#1368CE
+	    opt3_.style.cssText += "background-color: #3A9A1A";  //#26890C
+ 		opt4_.style.cssText += "background-color: #EAAA1A";  //#D89E00
+
+		this.rand = this.generateRandomNumber();
+		this.question = this.questionBank.results[questionNo];
+
+		switch(this.rand) {
+			case 0:
+			quest.innerText = (this.currentQuestion+1) +") "+ this.question.question;
+			opt1_.innerText = this.question.correct_answer;
+			opt2_.innerText = this.question.incorrect_answers[0];
+			opt3_.innerText = this.question.incorrect_answers[1];
+			opt4_.innerText = this.question.incorrect_answers[2];
+			break;
+			
+			case 1:
+			quest.innerText = (this.currentQuestion+1)+") "+ this.question.question;
+			opt1_.innerText = this.question.incorrect_answers[0];
+			opt2_.innerText = this.question.correct_answer;
+			opt3_.innerText = this.question.incorrect_answers[1];
+			opt4_.innerText = this.question.incorrect_answers[2];
+			break;
+			
+			case 2:
+			quest.innerText = (this.currentQuestion+1)+") "+ this.question.question;
+			opt1_.innerText = this.question.incorrect_answers[0];
+			opt2_.innerText = this.question.incorrect_answers[1]
+			opt3_.innerText = this.question.correct_answer;
+			opt4_.innerText = this.question.incorrect_answers[2];
+			break;
+			
+			case 3:
+			quest.innerText = (this.currentQuestion+1)+") "+ this.question.question;
+			opt1_.innerText = this.question.incorrect_answers[0];
+			opt2_.innerText = this.question.incorrect_answers[1];
+			opt3_.innerText = this.question.incorrect_answers[2];
+			opt4_.innerText = this.question.correct_answer;
+			break;
+		}
+
+	}
+
+	checkAnswer(opt) {
+		console.log(`opt:${opt}`);
+		console.log(`rand:${this.rand}`);
+		console.log(`ans:${this.answered}`);
+
+		if(opt == this.rand && this.answered == false) {
+			this.score++;
+			score_.innerText = `Score: ${this.score}/${this.numberOfquestions}`;
+
+			this.nextQuestion();
+			this.isElementHidden('next', true);
+			console.log('in if');
+		}
+		else {
+			this.showCorrectAnswer();
+			this.isElementHidden('next', false);
+			this.answered = true;
+			console.log('in else');
+		}	
+	}
+
+	showCorrectAnswer(){
+		// Wrong:FF3355   Correct:66BF39
+		switch(this.rand) {
+			case 0:
+			opt1_.style.cssText += "background-color: #6FBF3F";
+			opt2_.style.cssText += "background-color: #FF3F5F";
+			opt3_.style.cssText += "background-color: #FF3F5F";
+			opt4_.style.cssText += "background-color: #FF3F5F";
+			break;
+			
+			case 1:
+			opt1_.style.cssText += "background-color: #FF3F5F";
+			opt2_.style.cssText += "background-color: #6FBF3F";
+			opt3_.style.cssText += "background-color: #FF3F5F";
+			opt4_.style.cssText += "background-color: #FF3F5F";
+			break;
+			
+			case 2:
+			opt1_.style.cssText += "background-color: #FF3F5F";
+			opt2_.style.cssText += "background-color: #FF3F5F";
+			opt3_.style.cssText += "background-color: #6FBF3F";
+			opt4_.style.cssText += "background-color: #FF3F5F";
+			break;
+			
+			case 3:
+			opt1_.style.cssText += "background-color: #FF3F5F";
+			opt2_.style.cssText += "background-color: #FF3F5F";
+			opt3_.style.cssText += "background-color: #FF3F5F";
+			opt4_.style.cssText += "background-color: #6FBF3F";
+			break;
+		}
+	}
+
+	nextQuestion() {
+		if(this.currentQuestion < this.questionBank.results.length - 1)  {
+			this.displayQuestionAndOptions(++this.currentQuestion);
+			prog_.style.width = (100/ this.numberOfquestions) * this.currentQuestion +'%';	
+		} 
+		else {
+			this.displayResult();
+		}		
+	}
+
+	displayResult() {
+		this.isElementHidden('question', true);
+		this.isElementHidden('opt1', true);
+		this.isElementHidden('opt2', true);
+		this.isElementHidden('opt3', true);
+		this.isElementHidden('opt4', true);
+		this.isElementHidden('next', true);
+		this.isElementHidden('prev', true);
+
+		let r = this.score/this.numberOfquestions;
+		if(r > 0.8)
+			result_.innerText = "Great! ";
+		else if(r >0.4)
+			result_.innerText = "Well done! ";   
+		else
+			result_.innerText = " ";
+
+		prog_.style.width = '100%';
+		result_.innerText += `You Scored: ${this.score}/${this.numberOfquestions}`;
+	}
+	
+	previousQuestion() {
+		if(this.currentQuestion > 0) {
+			this.score -= 1;
+			score_.innerText = `Score: ${this.score}/${this.numberOfquestions}`;
+
+			this.displayQuestionAndOptions(--this.currentQuestion);	
+			prog_.style.width = (100/ this.numberOfquestions) * this.currentQuestion +'%';
+		}
+	}
+
+	isElementHidden(elementId ,isHidden) {
+		if(isHidden)
+			document.getElementById(elementId).hidden = true;
+		else
+			document.getElementById(elementId).hidden = false;
+	}
+
+	//Uncomment the following script tag in index.html before using the getTestQuestionBank() method
+	//<script src="test.js"></script>
+	getTestQuestionBank() {
+		return JSON.parse(testQuestions);
+	}
+
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
+// Use below code for testing
+function main() {
+	let form = new Quiz(null);
+	
+	// Setup onclick event handlers
+	opt1_.onclick = function() { form.checkAnswer(0); }
+	opt2_.onclick = function() { form.checkAnswer(1); }
+	opt3_.onclick = function() { form.checkAnswer(2); }
+	opt4_.onclick = function() { form.checkAnswer(3); }
+	document.getElementById('next').onclick = function() { form.nextQuestion();}
+	document.getElementById('prev').onclick = function() { form.previousQuestion();}
+
+	form.greetUser('Rutuparn');
+	form.displayQuestionAndOptions(form.currentQuestion);	
+}
+
+main();
+/////////////////////////////////////////////////////////////////////////////////////
